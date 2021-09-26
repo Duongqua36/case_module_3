1dd0129e3a9727e9696d55d70ed8e53d5578b84d
Duongqua36 <chuduong0306@gmail.com>
2021-09-26 20:01:51 +0700

    update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -42,7 +42,6 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-
                 <div class="our-link">
                     <ul>
                         <li><a href="#"><i class="fa fa-user s_color"></i> My Account</a></li>
